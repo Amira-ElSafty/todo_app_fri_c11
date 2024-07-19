ac21ed5866d0a8dd1531455509451b33c3aa1658
DESKTOP-NR6TVAI\LAPTOP <amiraelsaftytraining@gmail.com>
2024-07-19 14:59:58 +0300

    finished integrated firebase.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_todo_fri_c11/home/home_screen.dart';
 import 'package:flutter_app_todo_fri_c11/my_theme_data.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+              apiKey: 'AIzaSyBzpXLP_wJ9738JZHvDPhO-S8-j9owEN_o',
+              appId: 'com.example.flutter_app_todo_fri_c11',
+              messagingSenderId: '539679194519',
+              projectId: 'todo-app-fri-c11-66cb0'))
+      : await Firebase.initializeApp();
   runApp(MyApp());
 }
 
